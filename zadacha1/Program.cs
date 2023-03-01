85bf5c42abb59fdecaccd1c10ce4ed39ca4faece
Julia <kurkinajulia@gmail.com>
2023-03-01 16:38:26 +0300

    zadacha1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,21 +1,54 @@
-﻿//Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-//нельзя использовать Math.Pow
-//3, 5 -> 243 (3⁵)
-//2, 4 -> 16
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
 
 
-int Method(int number, int exponent)
+int Prompt(string messageTerminal)
 {
-    int result = 1;
-    for (int i = 0; i < exponent; i++)
-    {
-        result = result * number;
-    }
+    Console.Write(messageTerminal);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
     return result;
 }
-Console.Write("Введите число A: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число B: ");
-int exponent = Convert.ToInt32(Console.ReadLine());
-int res = Method(number, exponent);
-Console.WriteLine(res);
+
+int[] CreateArray(int size) //cоздание массива
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+
+void ShowArray(int[] array) //показ массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int even = 0;
+int ShowEvenNumbers(int[] array) //подсчёт количества чётных чисел в массиве
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            even++;
+    }
+    return even;
+}
+
+int size = Prompt("Введите размер массива: "); //введение пользователем размера массива 
+if (size <= 1)
+{
+    Console.WriteLine("Размер массива должен быть больше 1.");
+    return;
+}
+
+Console.Write("Массив заполнен числами: ");
+int[] array = CreateArray(size);
+ShowArray(array);
+ShowEvenNumbers(array);
+Console.WriteLine($"Количество чётных чисел в массиве: {even}");
