295cd815bd0187c81fb6905ec7a2d71e8bec0ead
Julia <kurkinajulia@gmail.com>
2023-03-01 16:34:28 +0300

    zadacha2

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -1,20 +1,54 @@
-﻿//Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
-//452 -> 11
-//82 -> 10
-//9012 -> 12
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
 
 
-int SumAllNumbers(int number)
+int Prompt(string messageTerminal)
 {
-    int result = 0;
-    while (number > 0)
-    {
-        result = result + number % 10;
-        number = number / 10;
-    }
+    Console.Write(messageTerminal);
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
     return result;
 }
-Console.Write("Введите число A: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int res = SumAllNumbers(number);
-Console.WriteLine(res);
+
+int[] CreateArray(int size) //cоздание массива
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+    return array;
+}
+
+void ShowArray(int[] array) //показ массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int sum = 0;
+int i = 0;
+int SumOddPosition(int[] array) //суммирование элементов, стоящих на нечётных позициях
+{
+    while (i < array.Length)
+    {
+        sum = sum + array[i + 1];
+        i += +2;
+    }
+    return sum;
+}
+
+int size = Prompt("Введите размер массива: "); //введение пользователем размера массива 
+if (size <= 1)
+{
+    Console.WriteLine("Размер массива должен быть больше 1.");
+    return;
+}
+
+Console.Write("Массив заполнен числами: ");
+int[] array = CreateArray(size);
+ShowArray(array);
+SumOddPosition(array);
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях: {sum}");
