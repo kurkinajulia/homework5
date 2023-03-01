5a69e90c9e6cb1d098269dc3eac2d54d586348e0
Julia <kurkinajulia@gmail.com>
2023-03-01 16:24:41 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//нельзя использовать Math.Pow
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+int Method(int number, int exponent)
+{
+    int result = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        result = result * number;
+    }
+    return result;
+}
+Console.Write("Введите число A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int exponent = Convert.ToInt32(Console.ReadLine());
+int res = Method(number, exponent);
+Console.WriteLine(res);
